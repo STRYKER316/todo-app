d342aa4fdc5cff0c10a1372a04bd2d7658546314
anshil <ansil.mishra316@gmail.com>
2021-09-21 13:40:35 +0530

    Add the 'Todo' data-model to our application

--- todo_app.py
+++ todo_app.py
@@ -11,13 +11,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # create an instance of the SQLAlchemy class and link it to the app
 db = SQLAlchemy(app)
 
+# data model for the app
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable= False)
+
+    def __repr__(self) -> str:
+        return f"<id {self.id}: {self.description}>"
+
+# sync all the newly created data model to the databse
+db.create_all()
+
 
 # Root route handler
 @app.route('/')
 def index():
-    return render_template('index.html', data = [
-        {'description': 'Todo 1'},
-        {'description': 'Todo 2'},
-        {'description': 'Todo 3'},
-        {'description': 'Todo 4'}
-    ])
+    return render_template('index.html', data = Todo.query.all())
